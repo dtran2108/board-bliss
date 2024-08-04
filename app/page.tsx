268b6ad5c49cafd7df5aca321bd764f3b1167818
Tran Duong <duong.tran@invincix.com>
2024-08-05 00:23:47 +0800

    add xstate todo machine

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-import Image from "next/image";
+'use client'
+
+import { todoMachine } from "@/machines/todoAppMachine";
+import { useMachine } from "@xstate/react";
 
 export default function Home() {
+  const [state, send] = useMachine(todoMachine);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello
+      {JSON.stringify(state.value)}
+      <button onClick={() => {
+        send({type: "Todos loaded"})
+      }}>Hover</button>
     </main>
   );
 }
