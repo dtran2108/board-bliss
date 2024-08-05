4dbf220981f9b689329a25ed9015a84c6354f094
Tran Duong <dtran2108@gmail.com>
2024-08-05 23:36:11 +0800

    remove xstate

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,8 @@
-'use client'
-
-import { todoMachine } from "@/machines/todoAppMachine";
-import { useMachine } from "@xstate/react";
-
 export default function Home() {
-  const [state, send] = useMachine(todoMachine);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {JSON.stringify(state.value)}
-      <button onClick={() => {
-        send({type: "Todos loaded"})
-      }}>todo loaded</button>
-      <button onClick={() => {
-        send({type: "Loading todos failed"})
-      }}>todo error</button>
+      hello
     </main>
   );
 }
